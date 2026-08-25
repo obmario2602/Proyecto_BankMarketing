1ecb061a609dc526eec6dea30bb28cc2c5dd09ca
Mario Ormeño <obmario2602o@gmail.com>
2026-08-25 07:13:18 -0500

    Actualizando la aplicación

--- app1.py
+++ app1.py
@@ -3,9 +3,8 @@ st.title("PROYECTO - BankMarketing")
 st.sidebar.title("Menú de navegación")
 Opciones = ["Inicio", "Carga del dataset"]
 Seleccion = st.sidebar.radio("Seleccione una opción", Opciones)
-if Opciones == "Inicio":
+if Seleccion == "Inicio":
     st.write("El objetivo del proyecto esanalizar los datos de la última campaña de una entidad financiera para descubrir relaciones y comportamientos relevantes entre las variables")
     st.write("Alumno: Mario Alberto Ormeño Bobadilla")
     st.write("Especialización en Python for Analytics")
     st.write("2026")
-    
