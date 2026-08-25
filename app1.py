24f578aeb3551252af114a61f103d66045c1b45a
Mario Ormeño <obmario2602o@gmail.com>
2026-08-25 07:05:33 -0500

    Actualizando la aplicación

--- app1.py
+++ app1.py
@@ -1,5 +1,10 @@
 import streamlit as st
 st.title("PROYECTO - BankMarketing")
 st.sidebar.title("Menú de navegación")
-Opciones = ["Inicio", "Análisis Exploratorio de Datos", "Modelos de Machine Learning", "Conclusiones"]
+Opciones = ["Inicio", "Carga del dataset"]
 Seleccion = st.sidebar.radio("Seleccione una opción", Opciones)
+if Opciones == "Inicio":
+    st.write("El objetivo del proyecto esanalizar los datos de la última campaña de una entidad financiera para descubrir relaciones y comportamientos relevantes entre las variables")
+    st.write("Alumno: Mario Alberto Ormeño Bobadilla")
+    st.write("Especialización en Python for Analytics")
+    st.write("2026")
