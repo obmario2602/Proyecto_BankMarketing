54095e43d36489fa783f856e77f5f1d9db1e8cbf
Mario Ormeño <obmario2602o@gmail.com>
2026-08-25 06:48:31 -0500

    Actualizando la aplicación

--- app1.py
+++ app1.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.title("PROYECTO - BankMarketing")
 st.sidebar.title("Menú de navegación")
+Opciones = ["Inicio", "Análisis Exploratorio de Datos", "Modelos de Machine Learning", "Conclusiones"]
+Seleccion = st.sidebar.radio("Seleccione una opción", Opciones)
