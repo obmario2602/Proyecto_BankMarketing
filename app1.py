b8368ad95ddca0fdff3d6bc88fccffd17a0bc3f9
Mario Ormeño <obmario2602o@gmail.com>
2026-08-25 21:19:56 -0500

    Actualizando la aplicación

--- app1.py
+++ app1.py
@@ -8,3 +8,4 @@ if Seleccion == "Inicio":
     st.write("Alumno: Mario Alberto Ormeño Bobadilla")
     st.write("Especialización en Python for Analytics")
     st.write("2026")
+    st.write("DATASET: Institución financiera que busca entender los factores que influyen en la aceptación de sus campañas de marketing. Durante los últimos 6 meses, la efectividad (e = (Ventas/Base)×100%) cayó de 12% a 8%, afectando los bonos de los ejecutivos comerciales.")
