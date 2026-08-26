b799154713ccff555c70a9148762d2f6be50af03
Mario Ormeño <obmario2602o@gmail.com>
2026-08-25 22:35:36 -0500

    Actualizando la aplicación

--- app1.py
+++ app1.py
@@ -12,7 +12,7 @@ if Seleccion == "Inicio":
     st.write("DATASET: Institución financiera que busca entender los factores que influyen en la aceptación de sus campañas de marketing. Durante los últimos 6 meses, la efectividad (e = (Ventas/Base)×100%) cayó de 12% a 8%, afectando los bonos de los ejecutivos comerciales.")
 elif Seleccion == "Carga del dataset":
     st.write("Cargar el dataset")
-    uploaded_file = st.file_uploader("C:\\Users\\Mario\\Desktop\\Prueba_final_DCM\\BankMarketing.csv", type="csv")
+    uploaded_file = st.file_uploader("Seleccione un archivo CSV", type=["csv"])
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         st.write("Vista previa del dataset:")
